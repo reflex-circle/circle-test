1c84fa7e2d16ba3b8af96f5779a2e5c9dd7d33ad
reflex-circle <reflex.circle@gmail.com>
2016-07-07 17:29:16 +0900

    Update tweet-destory.js

--- tweet-destory.js
+++ tweet-destory.js
@@ -3,13 +3,17 @@ var Twit = require('twit')
 require('date-utils');
 
 var dt = new Date();
-var instance = new Twit({
+var t = new Twit({
  consumer_key: process.env.CONSUMER_KEY,
  consumer_secret: process.env.CONSUMER_SECRET,
  access_token_key: process.env.ACCESS_TOKEN_KEY,
- access_token_secret: "aaa"//process.env.ACCESS_TOKEN_SECRET,
+ access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
 
+T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+  console.log(data)
+});
+/*
 instance.post('statuses/destroy/:id', { id: '750957884133896193' },
   function(err, data, response) {
    console.log(err);
@@ -17,6 +21,7 @@ instance.post('statuses/destroy/:id', { id: '750957884133896193' },
    console.log(response);
   }
 );
+*/
 
 /*
 frisby.create("SAMPLE TEST").get(
