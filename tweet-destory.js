73998752ce3681161c3bad8e18123f4fce5cc2a9
reflex-circle <reflex.circle@gmail.com>
2016-07-07 17:20:56 +0900

    Update tweet-destory.js

--- tweet-destory.js
+++ tweet-destory.js
@@ -7,7 +7,7 @@ var instance = new Twit({
  consumer_key: process.env.CONSUMER_KEY,
  consumer_secret: process.env.CONSUMER_SECRET,
  access_token_key: process.env.ACCESS_TOKEN_KEY,
- access_token_secret: "aaa";//process.env.ACCESS_TOKEN_SECRET,
+ access_token_secret: "aaa"//process.env.ACCESS_TOKEN_SECRET,
 });
 
 instance.post('statuses/destroy/:id', { id: '750957884133896193' },
