2487ab0c870457e3866b8a97de982b53f1b2dfc4
reflex-circle <reflex.circle@gmail.com>
2016-07-07 15:37:20 +0900

    Update test_spec.js

--- test_spec.js
+++ test_spec.js
@@ -34,7 +34,7 @@ var instance = new twitter({
  consumer_key: process.env.CONSUMER_KEY,
  consumer_secret: process.env.CONSUMER_SECRET,
  access_token_key: process.env.ACCESS_TOKEN_KEY,
- access_token_secret: 'process.env.ACCESS_TOKEN_SECRET,
+ access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
  
 // 自分のタイムラインの内容を取得
