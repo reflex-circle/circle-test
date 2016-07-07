b159074586447fc0b69bc4816ece536668e1a76c
reflex-circle <reflex.circle@gmail.com>
2016-07-07 17:41:29 +0900

    Update tweet-destory.js

--- tweet-destory.js
+++ tweet-destory.js
@@ -6,7 +6,7 @@ var dt = new Date();
 var t = new Twit({
  consumer_key: process.env.CONSUMER_KEY,
  consumer_secret: process.env.CONSUMER_SECRET,
- access_token_key: process.env.ACCESS_TOKEN_KEY,
+ access_token: process.env.ACCESS_TOKEN_KEY,
  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
 
