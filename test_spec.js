149fd584390c352aee3a7e1e55c36933c776eb7b
reflex-circle <reflex.circle@gmail.com>
2016-07-07 16:28:19 +0900

    Update test_spec.js

--- test_spec.js
+++ test_spec.js
@@ -1,7 +1,22 @@
 var frisby = require("frisby");
-var twitter = require('ntwitter');
-console.log(process.env.CONSUMER_KEY);
+var twitter = require("ntwitter");
+require('date-utils');
 
+var dt = new Date();
+var instance = new twitter({
+ consumer_key: process.env.CONSUMER_KEY,
+ consumer_secret: process.env.CONSUMER_SECRET,
+ access_token_key: process.env.ACCESS_TOKEN_KEY,
+ access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
+
+instance.updateStatus('Test By CircleCI ' + dt.toFormat('YYYY/MM/DD HH24:MI:SS'),
+  function(err, data) {
+   console.log(data);
+  }
+);
+
+/*
 frisby.create("SAMPLE TEST").get(
     // リクエストするAPIパス
     "https://api.twitter.com/1.1/statuses/user_timeline.json"
@@ -12,32 +27,5 @@ frisby.create("SAMPLE TEST").get(
     {"errors":[{"code":215,"message":"Bad Authentication data."}]}
 ).inspectJSON(
 ).toss();
-
-frisby.create("投稿").post(
-    "https://api.twitter.com/1.1/statuses/update.json",
-    {
-        status: 'test',
-        
-    }
-).expectStatus(
-    400
-).expectJSON(
-    // 期待するレスポンスボディ
-    {"errors":[{"code":215,"message":"Bad Authentication data."}]}
-).inspectJSON(
-).toss();
-
-
+*/
  
-// 各種キーを入力し、インスタンス生成
-var instance = new twitter({
- consumer_key: process.env.CONSUMER_KEY,
- consumer_secret: process.env.CONSUMER_SECRET,
- access_token_key: process.env.ACCESS_TOKEN_KEY,
- access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-});
- 
-// 自分のタイムラインの内容を取得
-instance.get('/statuses/home_timeline.json', {include_entities:true}, function(data) {
- console.log(data);
-});
